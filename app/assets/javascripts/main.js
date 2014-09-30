5402d86cf3c2136d9e7948dad49e7dd9fa8ebc0f
allison <azywong@gmail.com>
2014-09-30 16:33:09 -0700

    works.  done with jason exclamation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,12 @@
 var app = angular.module('quiz', []);
 
   app.controller('QuizCtrl', function($scope, $http){
-    $scope.currentQuizId = 0;
+    $scope.currentQuizId = 1;
     $scope.currentQuestion = {};
+    $scope.formData = {};
+    $scope.answerStatus = {};
 
-    $scope.getFirstQuiz = function(){
+    $scope.getFirstQuizId = function(){
       var self = this;
       $http.get('/quizzes.json').success(function(data){
         console.log(data.quizzes[0].quiz_id);
@@ -14,12 +16,43 @@ var app = angular.module('quiz', []);
 
     $scope.getNextQuestion = function(){
       var self = this;
-      $http.get('/quizzes/' + this.currentQuizId + '/questions/next').success(function(data){
+      $http({
+        url: '/quizzes/' + this.currentQuizId + '/questions/next.json',
+        method: 'GET',
+        params: {session_key: sessionId}
+      }).success(function(data){
         $scope.currentQuestion = data.question;
       });
     };
 
-    $scope.getFirstQuiz();
+    var getSession = function(){
+          var text = "";
+          var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+          for( var i=0; i < 5; i++ )
+              text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+          return text;
+      }
+
+    $scope.getFirstQuizId();
+    var sessionId = getSession();
+    console.log(sessionId);
+    $scope.getNextQuestion();
+
+    $scope.submitAnswer = function(answer){
+        $http({
+        url: '/questions/' + this.currentQuestion.question_id + '/answers.json',
+        method: 'POST',
+        params: {
+          session_key: sessionId,
+          choice_id: answer
+        }
+      }).success(function(data){
+        $scope.answerStatus[$scope.currentQuestion.question_id] = data.status.correct
+        $scope.getNextQuestion();
+      });
+    }
   });
 
 
